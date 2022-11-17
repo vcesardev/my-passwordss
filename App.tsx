150b6523357067a3f1039cf52fc7bfb7dde56cb7
Vitor Cesar <vitor@fasters.com.br>
2022-11-17 12:43:36 -0300

    feat: changes to app

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import AppLoading from "expo-app-loading";
 import { useFonts, Roboto_400Regular } from "@expo-google-fonts/roboto";
 import {
@@ -10,11 +9,9 @@ import {
 import { ThemeProvider } from "styled-components";
 import { theme as themeData } from "./src/theme/index";
 import { NavigationContainer } from "@react-navigation/native";
-import PageHeader from "./src/components/PageHeader";
-import Home from "./src/pages/App/Home";
-import Login from "./src/pages/Auth/Login";
 import { AuthProvider, useAuth } from "./src/hooks/auth";
 import Router from "./src/routes";
+import { PasswordProvider } from "./src/hooks/passwords";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -29,14 +26,16 @@ export default function App() {
     return (
       <NavigationContainer>
         <AuthProvider>
-          <ThemeProvider theme={themeData}>
-            <StatusBar
-              style="light"
-              backgroundColor={themeData.colors.purple}
-            />
+          <PasswordProvider>
+            <ThemeProvider theme={themeData}>
+              <StatusBar
+                style="light"
+                backgroundColor={themeData.colors.purple}
+              />
 
-            <Router />
-          </ThemeProvider>
+              <Router />
+            </ThemeProvider>
+          </PasswordProvider>
         </AuthProvider>
       </NavigationContainer>
     );
