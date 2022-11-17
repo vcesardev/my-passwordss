e95af57d09073b41e66335b10241037abc21753d
Vitor Cesar <vitor@fasters.com.br>
2022-11-17 12:42:30 -0300

    feat: add item to models

--- src/models/Password.ts
+++ src/models/Password.ts
@@ -2,4 +2,10 @@ export type BasePassword = {
   id: string;
   label: string;
   description: string;
+  userId: string;
+};
+
+export type PasswordPayload = {
+  label: string;
+  description: string;
 };
