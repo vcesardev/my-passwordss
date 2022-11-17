f9d12a28b24eee7f19445fc27e444611eb78b6bd
Vitor Cesar <vitor@fasters.com.br>
2022-11-17 12:43:08 -0300

    feat: add change to home page and its components

---
+++ src/pages/App/Home/components/NewPasswordModal/index.tsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import { Modal } from "react-native";
+import { PasswordPayload } from "../../../../../models/Password";
+
+import * as Styled from "./styled";
+
+type PasswordModalProps = {
+  onRequestClose: () => void;
+  isVisible: boolean;
+  onSendFormData: (data: PasswordPayload) => void;
+};
+
+const NewPasswordModal: React.FC<PasswordModalProps> = ({
+  isVisible,
+  onRequestClose,
+  onSendFormData,
+}) => {
+  const [label, setLabel] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+
+  const handleFormData = (): void => {
+    if (label.length < 1 || description.length < 1) {
+      return;
+    }
+
+    onSendFormData({ description: description, label: label });
+  };
+
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={isVisible}
+      onRequestClose={onRequestClose}
+    >
+      <Styled.ModalContainer>
+        <Styled.ModalBackground onPressIn={onRequestClose} />
+        <Styled.ContainerData>
+          <Styled.CloseIcon onPress={onRequestClose} />
+
+          <Styled.HeaderLabel>Adicione uma nova senha</Styled.HeaderLabel>
+
+          <Styled.InputLabel>Nome do campo</Styled.InputLabel>
+          <Styled.Input value={label} onChangeText={setLabel} />
+
+          <Styled.InputLabel>Informações</Styled.InputLabel>
+          <Styled.DescriptionInput
+            value={description}
+            onChangeText={setDescription}
+          />
+
+          <Styled.AddButton onPress={handleFormData}>
+            <Styled.ButtonText>Adicionar</Styled.ButtonText>
+          </Styled.AddButton>
+        </Styled.ContainerData>
+      </Styled.ModalContainer>
+    </Modal>
+  );
+};
+
+export default NewPasswordModal;
--- src/pages/App/Home/index.tsx
+++ src/pages/App/Home/index.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList } from "react-native";
 import PageHeader from "../../../components/PageHeader";
 import SafeKAV from "../../../components/SafeKAV";
+import { useAuth } from "../../../hooks/auth";
+import { usePasswords } from "../../../hooks/passwords";
+import { BasePassword, PasswordPayload } from "../../../models/Password";
 import { theme } from "../../../theme";
+import NewPasswordModal from "./components/NewPasswordModal";
 import PasswordItem from "./components/PasswordItem";
 import SearchInput from "./components/SearchInput";
 
 import * as Styled from "./styled";
 
 const Home: React.FC = () => {
+  const [passwordModalVisible, setPasswordModalVisible] = useState(false);
+  const { passwords, addPassword } = usePasswords();
+  const { user } = useAuth();
+
+  const handleModalDisplay = (): void => {
+    setPasswordModalVisible(!passwordModalVisible);
+  };
+
+  const handleFormData = async (data: PasswordPayload): Promise<void> => {
+    try {
+      addPassword(data);
+      handleModalDisplay();
+    } catch (e) {
+      return;
+    }
+  };
+
+  const showPasswordModal: React.FC = () => {
+    return (
+      <NewPasswordModal
+        isVisible={passwordModalVisible}
+        onRequestClose={handleModalDisplay}
+        onSendFormData={handleFormData}
+      />
+    );
+  };
+
   return (
     <SafeKAV>
       <Styled.Container>
@@ -24,18 +55,20 @@ const Home: React.FC = () => {
             </Styled.HeaderContainer>
 
             <Styled.PasswordsDataContainer>
-              <PasswordItem />
-              <PasswordItem />
-              <PasswordItem />
-              <PasswordItem />
+              <FlatList
+                data={passwords?.filter((password) => password.id !== user.id)}
+                renderItem={(item) => <PasswordItem />}
+                keyExtractor={(item) => item.id}
+              />
             </Styled.PasswordsDataContainer>
           </Styled.PasswordWrapper>
         </Styled.PasswordsContainer>
 
-        <Styled.NewPasswordContainer>
+        <Styled.NewPasswordContainer onPress={handleModalDisplay}>
           <Styled.AddIcon fill={theme.colors.white} />
         </Styled.NewPasswordContainer>
       </Styled.Container>
+      {passwordModalVisible && showPasswordModal()}
     </SafeKAV>
   );
 };
