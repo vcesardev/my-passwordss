667e2789f8aa2bc6af8eacc40ce8702d2f3597fd
Vitor Cesar <vitor@fasters.com.br>
2023-02-14 12:15:29 -0300

    feat: implement store to hooks

--- src/hooks/passwords.tsx
+++ src/hooks/passwords.tsx
@@ -1,13 +1,20 @@
-import React, { useState, useContext, createContext } from "react";
+import React, {
+  useState,
+  useContext,
+  createContext,
+  useCallback,
+  useEffect,
+} from "react";
 import { v4 as uuidv4 } from "uuid";
 
 import { BasePassword, PasswordPayload } from "../models/Password";
+import { retrievePasswords, storePasswords } from "../utils/store";
 import { useAuth } from "./auth";
 
 type PasswordProps = {
   passwords: BasePassword[];
   addPassword: (data: PasswordPayload) => void;
-  removePassword: (data: PasswordPayload) => void;
+  removePassword: (data: BasePassword) => void;
 };
 
 type PasswordProviderProps = {
@@ -24,28 +31,49 @@ export const PasswordProvider: React.FC<PasswordProviderProps> = ({
 
   const addPassword = async (data: PasswordPayload): Promise<void> => {
     try {
-      console.log(data);
       const previousPasswords = passwords;
       const payload: BasePassword = {
         ...data,
         userId: user.id,
         id: uuidv4(),
       };
-      console.log(payload);
+
       previousPasswords.push(payload);
-      console.log(previousPasswords);
+
+      await storePasswords(previousPasswords);
+
       setPasswords(previousPasswords);
     } catch (e) {
       console.log(e);
     }
   };
 
-  const removePassword = (data: BasePassword): void => {
+  const removePassword = async (data: BasePassword): Promise<void> => {
     const updatedPasswords = passwords.filter(
       (password) => password.id !== data.id
     );
+    await storePasswords(updatedPasswords);
     setPasswords(updatedPasswords);
   };
+
+  const loadUserPasswords = useCallback(async () => {
+    try {
+      if (!user) return;
+      const data = await retrievePasswords();
+
+      if (data) {
+        //await storeUserData(data);
+
+        setPasswords(data);
+      }
+    } catch (err: any) {
+      console.log(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    loadUserPasswords();
+  }, [loadUserPasswords]);
 
   return (
     <PasswordsContext.Provider
